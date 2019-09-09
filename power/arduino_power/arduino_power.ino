cb8a7772953b6edee54619dd27ac89421a76457a
ak <e33b1711@gmail.com>
2019-09-09 20:33:35 +0200

    tested without real heating system

--- power/arduino_power/arduino_power.ino
+++ power/arduino_power/arduino_power.ino
@@ -2,8 +2,8 @@
 const int sensorPin         = 4;          // digital input of the power meter pulses
 const int ledPin            = 13;         // LED output pin
 
-const int heatEnablePin     = 77;         // enable output for the triac 
-const int heatControlPin    = 88;         // analog output for the triac control
+const int heatEnablePin     = 10;         // enable output for the triac 
+const int heatControlPin    = 11;         // analog output for the triac control
 
 const int maxControlPower   = 6000;       // 100% Power of Heating
 const double powerIncrement = 6000 / 256; // one PWM step of heating Power
@@ -12,7 +12,6 @@ const int acceptablePower   = 6;          // acceptale Heating Draw from Grid
 bool sensorValue          = false; 
 bool oldValue             = false;
 long now;
-long recentFlank;
 long lastNegFlank;
 long lastPosFlank;
 
@@ -23,11 +22,14 @@ bool notFirstNegFlank     = false;
 
 int controlPower = 0;
 long offTime;
+bool systemError = false; 
+int incrUp       = 0;
 
 void setup() {
   
   //setup pins
-  pinMode(sensorPin, INPUT);
+  pinMode(sensorPin, INPUT_PULLUP);
+  
   pinMode(ledPin, OUTPUT);
   digitalWrite(heatEnablePin, LOW);
   pinMode(heatEnablePin, OUTPUT);
@@ -37,8 +39,8 @@ void setup() {
   Serial.begin(9600);
 
   //needed to supress wrong messages at startup
-  nextUpdate = millis();
-  lastNegFlank = millis();
+  lastNegFlank  = millis();
+  offTime       = millis() - 60000;
   
 }
 
@@ -46,25 +48,26 @@ void loop() {
   // read the value from the sensor, false = LED off / true = LED on !!!!!!
   now               = millis();
   oldValue          = sensorValue;
-  sensorValue       = digitalRead(sensorPin);
+  sensorValue       = !digitalRead(sensorPin);
+  digitalWrite(ledPin, sensorValue);
 
   //detect flanks and store times
   bool negFlank     = oldValue & !sensorValue;
   bool posFlank     = !oldValue & sensorValue;
-  recentFlank       = now;
 
   //a positive Flank just gets stored
   if (posFlank){
-    lastPosFlank    = recentFlank;
+    lastPosFlank    = now;
+    nextUpdate      = now;
   }
 
   //reset powerUpdate flag
    powerUpdate  = false;
 
   //a short pulse (<500ms) signals a 1/1000th of a kWh
-  if (negFlank & (recentFlank - lastPosFlank < 500)){
-    power             = 3600.0/(recentFlank - lastNegFlank);
-    lastNegFlank      = recentFlank;
+  if (negFlank & (now - lastPosFlank < 500)){
+    power             = 3600000.0/(now - lastNegFlank);
+    lastNegFlank      = now;
     powerUpdate       = notFirstNegFlank;
     notFirstNegFlank  = true;
   }
@@ -74,14 +77,14 @@ void loop() {
   if (sensorValue & (now - lastPosFlank > 500)){
     power = 0;
     powerUpdate     = nextUpdate < now;
-    nextUpdate      = now + 5000;   
+    if (powerUpdate) nextUpdate = now + 5000;   
   }
 
-  //a permanent off (> 60s) could be an error condition
-  if (!sensorValue & (now - lastNegFlank > 60000)){
-    power         = 99999;
+  //a permanent off (> 600s) could be an error condition
+  if (!sensorValue & (now - lastNegFlank > 600000)){
+    systemError   = true;
     powerUpdate   = nextUpdate < now;
-    nextUpdate    = now + 5000;   
+    if (powerUpdate) nextUpdate    = now + 5000;   
   }
 
   //control algorithm
@@ -90,27 +93,36 @@ void loop() {
     //calculate controlPower update
     int nxtControlPower = controlPower;
     if (power == 0){
-      nxtControlPower++;
+      incrUp++;
+      nxtControlPower+=incrUp;
     }
     if (power > 0){
+      incrUp=0;
       int powerDecrement  =  (power - acceptablePower) / powerIncrement;
       nxtControlPower = controlPower - powerDecrement; 
     }
-    if (nxtControlPower<0) controlPower = 0;
-    if (nxtControlPower>255) controlPower = 255;
+    if (nxtControlPower<0) nxtControlPower = 0;
+    if (nxtControlPower>=255) systemError = true;
+    if (systemError)  nxtControlPower = 0;
 
     //if controlPower becomes 0, get the time
     if (controlPower>0 & nxtControlPower==0) offTime = now;
+    controlPower = nxtControlPower;
 
     //write control and enable
+    bool h_enable = ( (controlPower>0) || (now-offTime < 60000) ) && !systemError;
     analogWrite(heatControlPin, controlPower); 
-    digitalWrite(heatControlPin, (controlPower>0) || (now-offTime < 60000));
+    digitalWrite(heatEnablePin, h_enable);
 
     //write uart
     Serial.print("P_grid:");
     Serial.println(int(power));
     Serial.print("P_heat:");
     Serial.println(int(controlPower*powerIncrement));
+    Serial.print("heating_enable:");
+    Serial.println(h_enable);
+    Serial.print("system_error:");
+    Serial.println(systemError);
     
   }
 
