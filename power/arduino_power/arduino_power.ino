608b5ef24a679eff7dcddc3bc28494b9f8e8baf9
ak <e33b1711@gmail.com>
2019-10-26 10:57:08 +0200

    arduino_power is a pure getter / setter now

--- power/arduino_power/arduino_power.ino
+++ power/arduino_power/arduino_power.ino
@@ -2,21 +2,14 @@
 const int sensorPin         = 4;                        // digital input of the power meter pulses (Utility power, only consumption)
 const int sensorPin1        = 5;                        // power to electric heating
 const int sensorPin2        = 6;                        // power from PV
-const int sensorPin3        = 8;                        // power export (eastron)
-const int sensorPin4        = 9;                        // power cumalitive ( sum(abs(L1-L3)) estron)
 const int ledPin            = 13;                       // LED output pin
+
+const int pwmPin            = 9;
+String inString             = "";
 
 const double meterConstant  = 3600000;                  // 1 mWs
 const double meterConstant1 = 1800000;                  // 0.5 mWs
 const double meterConstant2 = 1800000;                  // "
-const double meterConstant3 = 9000000;                  // 2.5 mWs
-const double meterConstant4 = 9000000;                  // "
-
-const int heatControlPin    = 11;                       // pwm output for controlling the power of the electric heating
-
-const int maxControlPower   = 3000;                     // 100% Power of Heating
-const double powerIncrement = maxControlPower / 256;    // one PWM step of heating Power
-const int acceptablePower   = 6;                        // acceptebale power drawn from Utility during electric heating
 
 long now;                                               //timestamp in milliseconds (now())
 
@@ -34,16 +27,6 @@ bool sensorValue2           = false;
 bool oldValue2              = false;
 long lastNegFlank2;
 long lastPosFlank2;
-
-bool sensorValue3           = false; 
-bool oldValue3              = false;
-long lastNegFlank3;
-long lastPosFlank3;
-
-bool sensorValue4           = false; 
-bool oldValue4              = false;
-long lastNegFlank4;
-long lastPosFlank4;
 
 int powerUtility;                                           //measured power values & update flags / counters & suppression of first update
 int errorUtility               = false;                     //error condition are not reversible and require a reset
@@ -63,34 +46,14 @@ bool powerUpdatePV          = false;
 long nextUpdatePV           = 0;
 bool killUpdate2            = false;
 
-int powerEx;
-bool errorEx                = false;
-bool powerUpdateEx          = false;
-long nextUpdateEx           = 0;
-bool killUpdate3            = false;
-
-int powerCu;
-bool errorCu                = false;
-bool powerUpdateCu          = false;
-long nextUpdateCu           = 0;
-bool killUpdate4            = false;
-
-int powerUt;
-bool errorUt                = false;
-bool powerUpdateUt          = false;
-
-int controlPower            = 0;                         //variables for the control algorithm
-bool powerUpdateControl     = false;                     //control power is in watt / powerIncrement
-int errorControl            = false;
-int incrUp                  = 0;
-
 void setup() {
   
   //setup pins
   pinMode(sensorPin, INPUT_PULLUP);
   
   pinMode(ledPin, OUTPUT);
-  pinMode(heatControlPin, OUTPUT);
+  pinMode(pwmPin, OUTPUT);
+  analogWrite(pwmPin, 0);
 
   //setup uart
   Serial.begin(9600);
@@ -104,6 +67,24 @@ void setup() {
 
 void loop() {
 
+  while (Serial.available() > 0) {
+    char inChar = Serial.read();
+
+      inString += (char)inChar;
+    
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n') {
+      if (inString.substring(0,4)=="pwm:"){
+        int val = inString.substring(4).toInt();
+        Serial.print("setPWM:");
+        Serial.println(val);
+        analogWrite(pwmPin, val);
+        // clear the string for new input:
+      }
+       inString = "";
+    }
+  }
+
   //get timestamp
   now               = millis();
   
@@ -111,10 +92,6 @@ void loop() {
   powerUpdateUtility    = false;
   powerUpdatePV         = false;
   powerUpdateHeating    = false;
-  powerUpdateControl    = false;
-  powerUpdateEx         = false;
-  powerUpdateCu         = false;
-  powerUpdateUt         = false;
   
   // read the value from the sensor, LED for debbuging
   oldValue          = sensorValue;
@@ -123,10 +100,6 @@ void loop() {
   sensorValue1       = digitalRead(sensorPin1);
   oldValue2          = sensorValue2;
   sensorValue2       = digitalRead(sensorPin2);
-  oldValue3          = sensorValue3;
-  sensorValue3       = digitalRead(sensorPin3);
-  oldValue4          = sensorValue4;
-  sensorValue4       = digitalRead(sensorPin4);
   digitalWrite(ledPin, sensorValue);
 
   //detect flanks
@@ -136,10 +109,6 @@ void loop() {
   bool posFlank1    = !oldValue1 & sensorValue1;
   bool negFlank2    = oldValue2 & !sensorValue2;
   bool posFlank2    = !oldValue2 & sensorValue2;
-  bool negFlank3    = oldValue3 & !sensorValue3;
-  bool posFlank3    = !oldValue3 & sensorValue3;
-  bool negFlank4    = oldValue4 & !sensorValue4;
-  bool posFlank4    = !oldValue4 & sensorValue4;
 
   //a positive flank just gets stored
   if (posFlank){
@@ -153,14 +122,6 @@ void loop() {
   if (posFlank2){
     lastPosFlank2       = now;
     nextUpdatePV        = now;
-  }
-  if (posFlank3){
-    lastPosFlank3       = now;
-    nextUpdateEx        = now;
-  }
-  if (posFlank4){
-    lastPosFlank4       = now;
-    nextUpdateCu        = now;
   }
 
    //a short pulse (<500ms) signals kWh increment
@@ -182,18 +143,6 @@ void loop() {
     powerUpdatePV         = killUpdate2;    //supress first update
     killUpdate2           = true;
   }
-  if (negFlank3 & (now - lastPosFlank3 < 500)){
-    powerEx               = meterConstant3/(now - lastNegFlank3);
-    lastNegFlank3         = now;
-    powerUpdateEx         = killUpdate3;    //supress first update
-    killUpdate3           = true;
-  }
-  if (negFlank4 & (now - lastPosFlank4 < 500)){
-    powerCu               = meterConstant4/(now - lastNegFlank4);
-    lastNegFlank4         = now;
-    powerUpdateCu         = killUpdate4;    //supress first update
-    killUpdate4           = true;
-  }
 
   //a permanent on (>500ms) signals 0 kWh (Utility counter only)
   if (sensorValue & (now - lastPosFlank > 500)){
@@ -212,16 +161,6 @@ void loop() {
     powerPV           = 0;
     powerUpdatePV     = nextUpdatePV < now;
     if (powerUpdatePV) nextUpdatePV = now + 90000;   
-  }
-    if (!sensorValue3 & (now - lastNegFlank3 > 200000)){
-    powerEx           = 0;
-    powerUpdateEx     = nextUpdateEx < now;
-    if (powerUpdateEx) nextUpdateEx = now + 200000;   
-  }
-  if (!sensorValue4 & (now - lastNegFlank4 > 200000)){
-    powerCu = 0;
-    powerUpdateCu     = nextUpdateCu < now;
-    if (powerUpdateCu) nextUpdateCu = now + 90000;   
   }
 
   /*
@@ -244,87 +183,12 @@ void loop() {
     powerUpdatePV         = nextUpdatePV < now;
     if (powerUpdatePV) nextUpdatePV = now + 90000;   
   }
-  if (sensorValue3 & (now - lastPosFlank3 > 2000)){
-    errorEx          = true;
-    powerUpdateEx    = nextUpdateEx < now;
-    if (powerUpdateEx) nextUpdateEx = now + 90000;   
-  }
-  if (sensorValue4 & (now - lastPosFlank4 > 2000)){
-    errorCu               = true;
-    powerUpdateCu         = nextUpdateCu < now;
-    if (powerUpdateCu) nextUpdateCu = now + 90000;   
-  }
-
-  //calculate Utility Power (negative and positive form eastron)
-  if (powerUpdateEx || powerUpdateCu){
-    powerUpdateUt  = true;
-    powerUt   = powerCu - 2*powerEx;
-  }
-  errorUt     = errorCu || errorEx;
-
-  //control algorithm, calculate update
-  if (powerUpdateUtility){
-    //calculate controlPower update
-    int nxtControlPower = controlPower;
-    if (powerUtility == 0){
-      incrUp++;
-      nxtControlPower+=incrUp;
-    }
-    if (powerUtility > 0){
-      incrUp=0;
-      int powerDecrement  =  (powerUtility - acceptablePower) / powerIncrement;
-      nxtControlPower = controlPower - powerDecrement; 
-    }
-    if (nxtControlPower<0) nxtControlPower = 0;
-    //if there is a error on the Utility sensor, we got also an error on the control
-    if (errorUtility || errorControl){
-      nxtControlPower = 0;
-      errorControl    = true;
-    }
-    //we got a update
-    powerUpdateControl      = true;
-    controlPower            = nxtControlPower;    
-  }
-
-/*
-  //control algorithm, check with measured power
-  if (powerUpdateHeating){
-    int powerDiff           = powerHeating - int(controlPower*powerIncrement);
-    if(powerDiff > 200 ||  powerDiff < -200){
-      //if the difference is too big we got an error
-      controlPower          = 0;
-      errorControl          = true;
-      powerUpdateControl    = true;
-    }
-  }
 
 
-  //control algorithm, check with pv power
-  if (powerUpdateHeating){
-    int powerDiff           = int(controlPower*powerIncrement) - powerPV;
-    if(powerDiff > 100){
-      //if the difference is bigger than zero (plus tolerance) we got an errror
-      controlPower            = 0;
-      errorControl            = true;
-      powerUpdateControl      = true;
-    }
-  }
-  */
 
 
-  //update the pmw output
-  if (powerUpdateControl){
-    //write control and enable
-    analogWrite(heatControlPin, controlPower);
-  }
-
-  //serial outputs
-  if (powerUpdateControl){
-    Serial.print("powerControl:");
-    Serial.println(controlPower);
-    Serial.print("errorControl:");
-    Serial.println(errorControl); 
-  }
+ 
+ 
   if (powerUpdateUtility){
     Serial.print("powerUtility:");
     Serial.println(powerUtility);
@@ -342,24 +206,6 @@ void loop() {
     Serial.println(powerHeating);   
     Serial.print("errorHeating:");
     Serial.println(errorHeating);  
-  }
-  if (powerUpdateEx){
-    Serial.print("powerEx:");
-    Serial.println(powerEx); 
-    Serial.print("errorEx:");
-    Serial.println(errorEx);    
-  }
-  if (powerUpdateCu){
-    Serial.print("powerCu:");
-    Serial.println(powerCu);   
-    Serial.print("errorCu:");
-    Serial.println(errorCu);  
-  }
-  if (powerUpdateUt){
-    Serial.print("powerUt:");
-    Serial.println(powerUt);   
-    Serial.print("errorUt:");
-    Serial.println(errorUt);  
   }
 
 }
