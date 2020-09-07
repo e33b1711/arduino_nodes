2d1dcfd0f889ea7126d0582e1d1ed8e4b73edddd
AK <e33b1711@gmail.com>
2020-09-07 19:22:51 +0200

    fix at ds temp sensors, still refactor and test

--- arduino_power_modbus/ds18b20_temp.ino
+++ arduino_power_modbus/ds18b20_temp.ino
@@ -5,7 +5,7 @@ float tempHigh, tempLow;
 DS18B20 ds(A7);
 
 long lastUpdateDS;
-const long updatePeriodDS = 5000;
+const long updatePeriodDS = 10000;
 
 
 
@@ -17,6 +17,31 @@ void setup_temp(){
    Serial.println("DS Temp Sensors...");
   Serial.print("Devices: ");
   Serial.println(ds.getNumberOfDevices());
+   while (ds.selectNext()) {
+    switch (ds.getFamilyCode()) {
+      case MODEL_DS18S20:
+        Serial.println("Model: DS18S20/DS1820");
+        break;
+      case MODEL_DS1822:
+        Serial.println("Model: DS1822");
+        break;
+      case MODEL_DS18B20:
+        Serial.println("Model: DS18B20");
+        break;
+      default:
+        Serial.println("Unrecognized Device");
+        break;
+    }
+     uint8_t address[8];
+    ds.getAddress(address);
+
+    Serial.print("Address:");
+    for (uint8_t i = 0; i < 8; i++) {
+      Serial.print(" ");
+      Serial.print(address[i]);
+    }
+    Serial.println();
+   }
    Serial.println("=========================");
 
   
@@ -28,22 +53,25 @@ void handle_temp(){
     if((lastUpdateDS+updatePeriodDS)<millis()){
       lastUpdateDS = millis();
 
-    ds.selectNext();
-    Serial.print("tempHigh: ");
-    tempHigh = ds.getTempC();
-    Serial.print(tempHigh);
-    Serial.println(" °C");
-
-    ds.selectNext();
-    Serial.print("tempLow: ");
-    tempLow = ds.getTempC();
-    Serial.print(tempLow);
-    Serial.println(" °C");
-  
-      
-
+     while (ds.selectNext()) {
+        uint8_t address[8];
+        ds.getAddress(address);
+        switch (address[7]){
+          case 51:
+            Serial.print("tempHigh: ");
+            tempHigh = ds.getTempC();
+            Serial.print(tempHigh);
+            Serial.println(" °C");
+            break;
+          case 189:
+            Serial.print("tempLow: ");
+            tempLow = ds.getTempC();
+            Serial.print(tempLow);
+            Serial.println(" °C");
+            break;
+          default:
+            Serial.println("ERROR: DS Address unknown."); 
+        }
+     } 
     }
-  
-  
-    
 }
