bc49e0a297bc2bd707198805087e9f862081d4e6
ak <e33b1711@gmail.com>
2020-09-09 20:24:40 +0200

    watchdog timer @ power_node

--- arduino_power_modbus/arduino_power_modbus.ino
+++ arduino_power_modbus/arduino_power_modbus.ino
@@ -90,6 +90,27 @@ void setup() {
   setup_log();
   setup_extern_log();
   setup_temp();
+  watchdogSetup();
+}
+
+void watchdogSetup(void){
+  cli(); // disable all interrupts
+  asm("WDR");
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+  WDTCSR =  (1<<WDIE) | (1<<WDE) | (1<<WDP3)| (1<<WDP0); // 8s / no interrupt, system reset
+  sei();
+}
+
+void watchdogOff(void){
+  cli(); // disable all interrupts
+  asm("WDR");
+  WDTCSR |= (0<<WDCE) | (0<<WDE);
+  WDTCSR =  (0<<WDIE) | (0<<WDE) | (0<<WDP3)| (0<<WDP0); // 8s / no interrupt, system reset
+  sei();
+}
+
+ISR(WDT_vect){
+  Serial.println(">>>>>>>>>>>>>>>>>>>>>HW Watchdog Interrupt<<<<<<<<<<<<<<<<<<<");
 }
 
 
