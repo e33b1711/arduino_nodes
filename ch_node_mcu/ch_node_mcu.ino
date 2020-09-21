3867c2a2bc3c3112c93f9882cf88cd95cc155418
ak <e33b1711@gmail.com>
2020-09-21 20:42:38 +0200

    CHicken house /deleted GR

--- ch_node_mcu/ch_node_mcu.ino
+++ ch_node_mcu/ch_node_mcu.ino
@@ -1,65 +1,51 @@
 //this nodes name
-const char* unit_name   = "gr";
+const char* unit_name   = "gr_nm";
 const char* password    = "pass";
+extern const char *ssid;
+extern const char *pass;
 
-
-//for tcp communication
+//for tcp communication (node mcu)
 //watch out for the pins needed for the ethernet schield (always 10, 11 12 13 on uno, 50 51 52 53 on mega!)
-#include <Ethernet.h>
-const byte mac[]                = {0xDE, 0xAA, 0x7E, 0xE1, 0x1E, 0x14 };
-const IPAddress                 ip(192,168,220,75);
+#include <WiFi.h>
+
 const IPAddress                 server(192,168,178,222);
-const IPAddress                 gateway(192,168,220,1);
-const IPAddress                 subnet(255,255,255,0);
 const int port                  = 8888;
-const int ethernet_sc_pin       = 10;
-const int ethernet_reset_pin    = 12;
+
+
 
 
 
 //constants and variables for b states (einer der 2 R-codierte Schalter an einem analogen Eingang)
-int counter_a=0;
-const int num_b_states  = 8;
-const int b_pin[]       = {26, 28, 30, 32, 34, 36, 38, 40};       //a state auf der selben unit                         
-int value_b[]           = {0, 0, 0, 0, 0, 0, 0, 0};       //an/aus
-int prev_value_b[]      = {0, 0, 0, 0, 0, 0, 0, 0};       //an/aus (alter Wert zur Flankenerkennung)
+int counter_a           = 0;
+const int num_b_states  = 4;
+const int b_pin[]       = {14, 27, 26, 25};       //a state auf der selben unit                         
+int value_b[]           = {0,  0,  0,  0};       //an/aus
+int prev_value_b[]      = {0,  0,  0,  0};       //an/aus (alter Wert zur Flankenerkennung)
 
 
 //constants and variables for c states (flanke eines b states)
 //0     taster innen
 //1     garagentor down
 //2     garagentor up
-//3     
-//4 
-//5
-//6  
-//7 
-//8 
-//9 
-//10
-//11 
-//12 
-//13 
-//14 
-//15 
-const int num_c_states=8;
-const int which_b[]={0, 1, 2, 3, 4, 5, 6, 7};       //a state auf der selben unit                         
-int value_c[]={0, 0, 0, 0, 0, 0, 0, 0};   //positive flanke
-int aux_value_c[]={0, 0, 0, 0, 0, 0, 0, 0};        //negative flanke
-long time_c_neg[]={0, 0, 0, 0, 0, 0, 0, 0};        //zeit der letzen fallenden flanke
-long time_c_pos[]={0, 0, 0, 0, 0, 0, 0, 0};        //zeit der letzen steigenden flanke
+const int num_c_states  = 4;
+const int which_b[]     = {0,  1,  2,  0};        //a state auf der selben unit                         
+int value_c[]           = {0,  0,  0,  0};        //positive flanke
+int aux_value_c[]       = {0,  0,  0,  0};        //negative flanke
+long time_c_neg[]       = {0,  0,  0,  0};        //zeit der letzen fallenden flanke
+long time_c_pos[]       = {0,  0,  0,  0};        //zeit der letzen steigenden flanke
 
 
 //constants and variables for t states (temperatur über dht22 an digitalem pin)
 #define DHTTYPE DHT22
-const int num_t_states=2;
-const long period_t=1800000;                                                                                  //update periode in ms
-const String t_address[]={"TI_GR", "TI_GR_A"};                                                                                                           //addresse
-const int t_pin[]={ 22, 24};
-int value_t[]={ 0,   0};                                            //temperatur
-int aux_value_t[]={  0,   0};                                            //feuchtigkeit
-long s_time_t=0;                                                                                          //update timer
-int i_t=0;                                                                                              //cycle_counter
+#define DHTPIN1 22
+#define DHTPIN2 23
+const int num_t_states      = 2;
+const long period_t         = 1800000;                                                                                  //update periode in ms
+const String t_address[]    = {"TI_GR", "TI_GR_A"};                                                                                                           //addresse
+int value_t[]               = {0,  0};                                            //temperatur
+int aux_value_t[]           = {0,  0};                                            //feuchtigkeit
+long s_time_t               = 0;                                                                                          //update timer
+int i_t                     = 0;                                                                                              //cycle_counter
   
 
 //constants and variables for h states (feuchtigkeit über zustand t über dht22 an digitalem pin)
@@ -74,12 +60,12 @@ int value_h[]={0, 0};
  * 1 aussen
  * ...
  */
-const int num_l_states      = 8;
-const String l_address[]    = {"LI_GR", "LI_GR_L1", "LI_GR", "ZE_GR_0", "ZE_GR_1", "ZE_GR_2", "DO_GR_DO", "DO_GR_UP" };       //addresse, zum gleichschalten selbe addresse vergeben
-const int l_pin[]           = {23, 25, 27, 29, 31, 33, 35, 37};                //digitaler pin
-const bool l_inv[]          = {0,  0,  0,  0,  0,  0,  0,  0};                //digitaler pin
-int value_l[]               = {0,  0,  0,  0,  0,  0,  0,  0};
-long set_time_l[]           = {0,  0,  0,  0,  0,  0,  0,  0};
+const int num_l_states      = 11;
+const String l_address[]    = {"LI_GR", "LI_GR", "LI_GR_L1", "LI_GR_L2", "LI_GR_L3", "LI_GR_L4", "DO_GR_UP", "DO_GR_DO", "ZE_GR_0", "ZE_GR_1", "ZE_GR_2"};       //addresse, zum gleichschalten selbe addresse vergeben
+const int l_pin[]           = {0,  4, 21,  16, 17, 5,  18, 19, -1, -1, -1};                //digitaler pin
+const bool l_inv[]          = {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};                //digitaler pin
+int value_l[]               = {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+long set_time_l[]           = {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
 
 
 
@@ -99,25 +85,25 @@ const long lock_delay_r           = 20000;
 
 ////constants and variables for s states (dachfenster)
 // üer eingänge auf und ab gesteuert
-const int num_s_states=0;
-const String s_address[]={};       //addresse
-const String s_up[]={      };         //l state
-const String s_down[]={  };         //l state
-const int up_time_s[]={      };       // zeit zum öffnen in ms
-const int down_time_s[]={    };       // zeit zum schließen in ms
-int value_s[]={               };          // -1 zu und verriegelt, 0 entriegelt, 1 auf und verriegelt
-long stop_time_s[]={         };          // zeit zu stoppen
+const int num_s_states          = 0;
+const String s_address[]        = {};       //addresse
+const String s_up[]             = {};         //l state
+const String s_down[]           = {};         //l state
+const int up_time_s[]           = {};       // zeit zum öffnen in ms
+const int down_time_s[]         = {};       // zeit zum schließen in ms
+int value_s[]                   = {};          // -1 zu und verriegelt, 0 entriegelt, 1 auf und verriegelt
+long stop_time_s[]              = {};          // zeit zu stoppen
 
   
   
 //constants and variables for u states (temperatur steller)
-const int num_u_states=0;
-const String u_address[]={ };        //addresse
-const int u_pin[]={ };               //pwm-pin  
-const unsigned long u_interval= 240000;          //pwm periode /16 in milisekunden
-unsigned long previousMillis=0;
-int u_phase=0;
-int value_u[]={};               //stell wert 0-15 (0=aus bis 15=voll)
+const int num_u_states          = 0;
+const String u_address[]        = {};        //addresse
+const int u_pin[]               = {};               //pwm-pin  
+const unsigned long u_interval  = 240000;          //pwm periode /16 in milisekunden
+unsigned long previousMillis    = 0;
+int u_phase                     = 0;
+int value_u[]                   = {};               //stell wert 0-15 (0=aus bis 15=voll)
 
   
 
@@ -140,7 +126,7 @@ void user_logic(){
   prev_value = address_to_value("DO_GR");
 
   //timer innenbeleuchtung lang (taster)
-  i=3;
+  i=8;
   static boolean timer_0_on=false;
   //start
   if ((address_to_value("ZE_GR_0")==1) & (timer_0_on==false)){
@@ -166,7 +152,7 @@ void user_logic(){
 
 
   //timer innenbeleuchtung kurz (einfahrt)
-   i=4;
+   i=9;
   static boolean timer_1_on=false;
   //start
   if ((address_to_value("ZE_GR_1")==1) & (timer_1_on==false)){
@@ -192,7 +178,7 @@ void user_logic(){
 
 
   //timer außen, mit vordach timer auslösen
-  i=5;
+  i=10;
   static boolean timer_2_on=false;
   //start
   if ((address_to_value("ZE_GR_2")==1) & (timer_2_on==false)){
