2f467883909775074d5b2cc727f8b1c7f150a74a
AK <e33b1711@gmail.com>
2021-01-05 14:15:01 +0100

    ...

--- arduino_power_modbus/arduino_power_modbus.ino
+++ arduino_power_modbus/arduino_power_modbus.ino
@@ -1,24 +1,8 @@
 /*TODO
-Status:
-modbus Error (no response)
-Heating Error (no powerHeat when pwm)
-s0 Error (balPower>0 and powerImport=0, balPower<0 and powerPV=0)
-no ntp time 
-=> non-persistent status
-=> persistent status (later resetable)
-=> check all dependencies of status <> functions
-
-Control:
-=> set target power / control mode over serial (done)
-=> set target power / control mode over mqtt
-=> reset status over mqtt
-=> reset status over debug
-=> direct mode (control off) (done)
-
-write long time statistics, WIP, new day!!
-
-lower cutoff power heating / how to prevent toggling?
-
+=> mqtt "inputs"
+=> energy integrator (from balanced power => saldo in/ex energy)
+=> water warning
+=> extern logging of energy
 */
 
 
@@ -27,21 +11,6 @@ const char* unit_name  = "power";
 const char* password   = "pass";
 const char* vers       = "v0.88                 ";
 
-
-//log
-struct log_entry{
-    bool day_intact;         //data started at 00:00 or stems form a log_entry with day_intact=true;
-    long epoch;
-    long pulseCount0;
-    long pulseCount1;
-    long pulseCount2;
-    float unsalEnergyImportZero;
-    float unsalEnergyExportZero;
-};
-void clear_eeprom();
-void readback_log();
-void readback_log(int val);
-void new_day_log();
 
 //heat
 extern int pwm_setpoint;
@@ -70,11 +39,6 @@ extern float energyExport,lastEnergyExport;
 extern float unsalEnergyImportZero;
 extern float unsalEnergyExportZero;
 
-
-//ntp / time related
-extern unsigned long epoch;                      //epoch updated every cycle
-extern unsigned long seconds_today;              //recent uninx seconds of this day
-extern unsigned long unix_day;                    // this unix day 
 
 
 //temp
