19ef10329882a7f7700a1c74fbb04a89a2b8c0fa
ak <e33b1711@gmail.com>
2022-03-25 18:20:37 +0100

    arduino_power_modbus: make it use tcp standard protocol / pwm setter with overtemp and timeout

--- arduino_power_modbus/arduino_power_modbus.ino
+++ arduino_power_modbus/arduino_power_modbus.ino
@@ -1,131 +1,58 @@
-/*TODO
-Status:
-modbus Error (no response)
-Heating Error (no powerHeat when pwm)
-s0 Error (balPower>0 and powerImport=0, balPower<0 and powerPV=0)
-no ntp time 
-=> non-persistent status
-=> persistent status (later resetable)
-=> check all dependencies of status <> functions
-
-Control:
-=> set target power / control mode over serial (done)
-=> set target power / control mode over mqtt
-=> reset status over mqtt
-=> reset status over debug
-=> direct mode (control off) (done)
-
-write long time statistics, WIP, new day!!
-
-lower cutoff power heating / how to prevent toggling?
-
-*/
-
 
 //unit's stuff
 const char* unit_name  = "power";
 const char* password   = "pass";
-const char* vers       = "v0.88                 ";
+
+//for tcp communication
+//watch out for the pins needed for the ethernet schield (always 10, 11 12 13 on uno, 50 51 52 53 on mega!)
+#include <Ethernet.h>
+const byte mac[]                = {0x4E, 0xAB, 0x7E, 0xEF, 0xFE, 0x04 };
+const IPAddress                 ip(192,168,178,213);
+const IPAddress                 server(192,168,178,222);
+const IPAddress                 gateway(192,168,178,1);
+const IPAddress                 subnet(255,255,255,0);
+const int port                  = 8888;
+const int ethernet_sc_pin       = 53;
+const int ethernet_reset_pin    = 12;
 
 
-//log
-struct log_entry{
-    bool day_intact;         //data started at 00:00 or stems form a log_entry with day_intact=true;
-    long epoch;
-    long pulseCount0;
-    long pulseCount1;
-    long pulseCount2;
-    float unsalEnergyImportZero;
-    float unsalEnergyExportZero;
-};
-void clear_eeprom();
-void readback_log();
-void readback_log(int val);
-void new_day_log();
-
-//heat
-extern int pwm_setpoint;
-extern unsigned long watchdog_counter;
-extern float target_power;
-extern int control_mode;
-extern void heat_off();
+//constants and variables for t states (temperatur über dht22 an digitalem pin)
+#define DHTTYPE DHT22
+const int num_t_states      = 2;
+const long period_t         = 100000;                                            //update periode in ms
+const String t_address[]    = {"TI_PU_O", "TI_PU_U"};                                    
+int value_t[]               = {0,  0};                                            //temperatur
+long lastUpdateDS_t;
+const long updatePeriodDS_t = 10000;
 
 
-//modbus
-extern float bal_power;
-extern bool bal_power_valid;
-extern float sdm_data[];
-extern bool sdm_data_valid;
+//constants and variables for p states (pwm output)
+const int num_p_states      = 1;
+const String p_address[]    = {"P_EL"};
+const int p_pin[]           = {5};                //digitaler pin
+unsigned short value_p[]    = {0};    
+long set_time_p[]           = {0};    
+unsigned short  max_p[]     = {210};
+
+//TODO: wheres the water warning?
+
+void user_logic()
+{
+
+    //timeout for elo heating
+    const long heat_timeout_c = 30000;
+    if ( (set_time_p[0]+heat_timeout_c) < millis() and (address_to_value("P_EL")!=0)){
+        write_p("P_EL", 0);
+        send_message("info", String(unit_name) + " heat timeout", 0);
+    }
+    
 
 
-//S0
-extern float powerUtility, powerHeat, powerPV;
-float energyImport();
-float energyHeat();
-float energyPV();
-extern long pulseCount0;
-extern long pulseCount1;
-extern long pulseCount2;
-extern float energyExport,lastEnergyExport;
-extern float unsalEnergyImportZero;
-extern float unsalEnergyExportZero;
+    //overtemp for elo heating, 85°
+    const int max_temp_c = 85;
+    if ( (address_to_value("TI_PU_O") > max_temp_c) and (address_to_value("P_EL")!=0) ){
+        write_p("P_EL", 0);
+        send_message("info", String(unit_name) + " overtemp", 0);
+    }
 
-
-//ntp / time related
-extern unsigned long epoch;                      //epoch updated every cycle
-extern unsigned long seconds_today;              //recent uninx seconds of this day
-extern unsigned long unix_day;                    // this unix day 
-
-
-//temp
-extern float tempHigh, tempLow;
-
-
-void setup() {
-  setup_debug();
-  setup_server();
-  //setup_time() --at server
-  setup_modbus();
-  setup_s0();
-  setup_heating();
-  setup_log();
-  setup_extern_log();
-  setup_temp();
-  watchdogSetup();
-}
-
-void watchdogSetup(void){
-  cli(); // disable all interrupts
-  asm("WDR");
-  WDTCSR |= (1<<WDCE) | (1<<WDE);
-  WDTCSR =  (1<<WDIE) | (1<<WDE) | (1<<WDP3)| (1<<WDP0); // 8s / no interrupt, system reset
-  sei();
-}
-
-void watchdogOff(void){
-  cli(); // disable all interrupts
-  asm("WDR");
-  WDTCSR |= (0<<WDCE) | (0<<WDE);
-  WDTCSR =  (0<<WDIE) | (0<<WDE) | (0<<WDP3)| (0<<WDP0); // 8s / no interrupt, system reset
-  sei();
-}
-
-ISR(WDT_vect){
-  heat_off();
-  Serial.println(">>>>>>>>>>>>>>>>>>>>>HW Watchdog Interrupt<<<<<<<<<<<<<<<<<<<");
-}
-
-
-void loop() {
-  handle_time();
-  asm("WDR");
-  handle_server();
-  asm("WDR");
-  update_heating();
-  update_s0();
-  handle_debug();
-  handle_modbus();
-  handle_log();
-  handle_extern_log();
-  handle_temp();
 }
--- arduino_power_modbus/heating.ino
+++ arduino_power_modbus/heating.ino
@@ -1,120 +1,36 @@
-const int pwmPin                      = 5;
-unsigned long heatingLastUpdate;
-const float c_down                    = 1.0/10;
-const float c_up                      = 1.0/15;
-const float c_hist                    = 25;   //hystereses
-const float c_pwm_max                 = 240;
-const float max_temp                  = 85;
-const unsigned long time_out          = 10000; 
-const unsigned long h_update_period   = 1000;
-float target_power                    = -25;
-int control_mode                      =  1; //0=off, 1=control balanced power, 2=control heat power
-int pwm_setpoint                      = 0;
-unsigned long watchdog_counter        = 0;
-bool h_control_off                    = false;
-
-void setup_heating(){
-    Serial.println("===============================");
-    pinMode(pwmPin, OUTPUT);
-    analogWrite(pwmPin, 0);
-
-    Serial.println("Setting up heating.");
-    /*
-    Serial.print("Switching on...");
-    analogWrite(pwmPin, 150);
-    delay(4000);
-    Serial.println(" and off.");
-    analogWrite(pwmPin, 0);
-    */
-    heatingLastUpdate = millis();
-    Serial.println("===============================");
-}
-
-void heat_off(){
-  pinMode(pwmPin, OUTPUT);
-  analogWrite(pwmPin, 0);
+void setup_p()
+{
+  Serial.println("setup_l");
+  int i;
+  for (i=0; i<num_p_states; i++){
+    pinMode(p_pin[i],OUTPUT);
+    analogWrite(p_pin[i], 0);
+    value_p[i]=0;
+    send_message("w", p_address[i], value_p[i]);
+    set_time_p[i]=millis();
+  }
 }
 
 
-//called form main loop
-void update_heating(){
-
-    float pwm_set_point_new = 0;
-
-    //try to get a modbus update and regulate the power setting
-    if (heatingLastUpdate+h_update_period<millis()){
-        //reset watchtdog timer
-        heatingLastUpdate = millis();
-      
-        //default is off
-        int pwm_setpoint_new= 0;
-
-        //mode 1 control
-        if (modbus_get_bal_power() and control_mode==1){
-      
-            // P-Controller with up/down weigth
-            float power_diff = target_power-bal_power;
-            //Serial.print("DEBUG: power_diff:"); Serial.println(power_diff);
-
-            if (power_diff>c_hist){
-            pwm_setpoint_new = (power_diff*c_up) + pwm_setpoint;
-            }
-            //Serial.print("DEBUG: (power_diff*c_up):"); Serial.println((power_diff*c_up));
-            //Serial.print("DEBUG: pwm_setpoint_new:"); Serial.println(pwm_setpoint_new);
-            if (power_diff<c_hist){
-            pwm_setpoint_new = (power_diff*c_down) + pwm_setpoint;
-            }
-            //Serial.print("DEBUG: (power_diff*c_down):"); Serial.println((power_diff*c_down));
-            //Serial.print("DEBUG: pwm_setpoint_new:"); Serial.println(pwm_setpoint_new);
-
-            //saturation
-            if (pwm_setpoint_new >c_pwm_max)  pwm_setpoint_new = c_pwm_max;
-            if (pwm_setpoint_new <0)    pwm_setpoint_new = 0;
-            //Serial.print("DEBUG: pwm_setpoint_new:"); Serial.println(pwm_setpoint_new);
-
-        }
-
-        //mode 2 set pwm direct
-        if (control_mode==2){
-            pwm_setpoint_new = int(target_power);
-            //saturation
-            if (pwm_setpoint_new >c_pwm_max)  pwm_setpoint_new = c_pwm_max;
-            if (pwm_setpoint_new <0)    pwm_setpoint_new = 0;
-        }
-        pwm_setpoint = pwm_setpoint_new;
-    }
+void update_p()
+{
+  int i;
+  for (i=0; i<num_p_states; i++){
+  if (value_p[i] > max_p[i]) value_p[i] = max_p[i];
+    analogWrite(p_pin[i], value_p[i]);
+  }
   
-
-  //watchdog
-    if (heatingLastUpdate+time_out<millis()){
-        watchdog_counter++;
-        Serial.println("ERROR: Heating watchdog barked!");
-        Serial.print("Watch dogcounter: ");
-        Serial.println(watchdog_counter);
-        heatingLastUpdate = millis();
-        pwm_setpoint = 0;
-    }
-
-    //overheat protection
-    if (tempHigh > max_temp){
-        Serial.println("ERROR: Overtemperature. Setting heating to 0!");
-        pwm_setpoint = 0;
-    }
-  
-    //write pwm
-    analogWrite(pwmPin, pwm_setpoint);
 }
 
 
-void print_heating_info(){
-    Serial.println("=============HEATING INFO==========");
-    Serial.print("target_power: ");
-    Serial.println(target_power);
-    Serial.print("control_mode: ");
-    Serial.println(control_mode);
-    Serial.print("PWM setpoint:  ");
-    Serial.println(pwm_setpoint);
-    Serial.print("Watch dog counter: ");
-    Serial.println(watchdog_counter);
-    Serial.println("===================================");
+void write_p(String address, int value){
+  int i;
+  for (i=0; i<num_p_states; i++){
+    if (p_address[i]==address){
+      set_time_p[i]=millis();
+      value_p[i] = value;
+      if (value_p[i] > max_p[i]) value_p[i] = max_p[i];
+      send_message("w", p_address[i], value_p[i]); 
+    }
+  }
 }
